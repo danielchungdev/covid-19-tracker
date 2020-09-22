1661c40cc22e7c7eb3c05d57a1b313dfba475aa1
pikachungg <34949943+pikachungg@users.noreply.github.com>
2020-09-21 23:51:44 -0400

    Updates

---
+++ precache-manifest.4993eda8e24d7b1467394b2b05700aad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a23ca2befc711b1620a3ae351e2d2a",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "2130a21870c0e85b6286",
+    "url": "/covid-19-tracker/static/css/2.067ccc96.chunk.css"
+  },
+  {
+    "revision": "f0c0e7b3f8d5aa5e9977",
+    "url": "/covid-19-tracker/static/css/main.cbe05520.chunk.css"
+  },
+  {
+    "revision": "2130a21870c0e85b6286",
+    "url": "/covid-19-tracker/static/js/2.490bf0f3.chunk.js"
+  },
+  {
+    "revision": "09b7b0e7282c26f7dc613590d311ab01",
+    "url": "/covid-19-tracker/static/js/2.490bf0f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c0e7b3f8d5aa5e9977",
+    "url": "/covid-19-tracker/static/js/main.f4802444.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
